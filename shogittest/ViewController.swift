acb6dea9efd4d4e2f88baa38633fe442f84f2226
張翔 <sho@choushou-no-MacBook-Air.local>
2015-12-02 19:04:36 +0900

    commit

---
+++ shogittest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  shogittest
+//
+//  Created by 張翔 on 2015/12/02.
+//  Copyright © 2015年 sho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func button(){
+        
+    }
+
+
+}
+
